e2243a5465d2268dc13c391b30a65747c018397e
Takayuki Sakai <kaky0922@gmail.com>
2015-02-07 16:04:36 +0900

    待ったボタンクラス追加

--- HasamiShogi/MattaButton.swift
+++ HasamiShogi/MattaButton.swift
@@ -6,4 +6,22 @@
 //  Copyright (c) 2015 Takayuki Sasai. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
+
+class MattaButton : SKLabelNode {
+    
+    init(text: String) {
+        super.init()
+        self.text = text
+        self.fontSize = 24
+        self.userInteractionEnabled = true
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        println(text + " button was tapped")
+    }
+}
