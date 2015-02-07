d067daf95c0ad0b3e4729c84cddba5e2c99f6959
Takayuki Sakai <kaky0922@gmail.com>
2015-02-07 16:05:43 +0900

    マスごとにタッチイベントを取れるようにした

--- HasamiShogi/Masu.swift
+++ HasamiShogi/Masu.swift
@@ -6,4 +6,26 @@
 //  Copyright (c) 2015 Takayuki Sasai. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
+
+class Masu {
+    let x:Int
+    let y:Int
+    var coord:Coordinate
+    let scene: GameScene
+    var node: Masu_SKSpriteNode?
+    var selected: Bool = false
+    
+    init(x: Int, y: Int, coord: Coordinate, scene: GameScene) {
+        self.x = x
+        self.y = y
+        self.coord = coord
+        self.scene = scene
+        
+        self.node = Masu_SKSpriteNode(masu: self, imageNamed:"koma_ho")
+    }
+    
+    func touched() {
+        println("koma at (" + self.x.description + ", " + self.y.description + ") was touched!")
+    }
+}
--- HasamiShogi/Masu_SKSpriteNode.swift
+++ HasamiShogi/Masu_SKSpriteNode.swift
@@ -6,4 +6,22 @@
 //  Copyright (c) 2015 Takayuki Sasai. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
+
+class Masu_SKSpriteNode : SKSpriteNode {
+    let masu: Masu
+    
+    init(masu: Masu, imageNamed: String) {
+        self.masu = masu
+        super.init(texture: SKTexture(imageNamed: imageNamed), color: UIColor.clearColor(), size: CGSizeMake(32, 32))
+        self.userInteractionEnabled = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.masu.touched()
+    }
+}
