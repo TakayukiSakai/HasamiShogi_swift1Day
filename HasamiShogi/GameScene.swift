870e0d248dc5be382e5236276078b84e153ca574
Takayuki Sakai <kaky0922@gmail.com>
2015-02-07 12:17:54 +0900

    jsonファイルでconfig読み込む

--- HasamiShogi/GameScene.swift
+++ HasamiShogi/GameScene.swift
@@ -10,35 +10,28 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        let path : String! = NSBundle.mainBundle().pathForResource("config", ofType: "json")
+        let fileHandle : NSFileHandle! = NSFileHandle(forReadingAtPath: path)
+        let data : NSData = fileHandle.readDataToEndOfFile()
+        var config: NSDictionary = NSJSONSerialization.JSONObjectWithData(data,
+            options: NSJSONReadingOptions.AllowFragments,
+            error: nil) as NSDictionary
         
-        self.addChild(myLabel)
+        println(config.objectForKey("masu")!.objectForKey("width")!)
+        
+        let masu = SKSpriteNode(imageNamed:"00_masu")
+        masu.position = CGPoint(
+            x:CGRectGetMidX(self.frame),
+            y:CGRectGetMidY(self.frame)
+        )
+        
+        self.addChild(masu)
+        println(masu.size.width)
+        
+        let myBoundSize: CGSize = UIScreen.mainScreen().bounds.size
+        println("Bounds width: \(myBoundSize.width) height: \(myBoundSize.height)")
     }
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
-    }
-   
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
